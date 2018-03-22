ee5f93de6d90b1a0d0dc53e7ddfc053e07c322aa
David Blood <davidbjourno@users.noreply.github.com>
2018-03-22 12:05:03 +0000

    Update JS for 12 questions

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -122,7 +122,7 @@ app.controller('QuestionCtrl', ['$scope', '$timeout', '$window', '$http',
       }
 
       // Update progress bar
-      const progress = ($scope.currentQuestion.value + 1) * 10;
+      const progress = ($scope.currentQuestion.value + 1) * (100 / $scope.questions.length);
       document.querySelector('.progress-bar').style.width = `${progress}%`;
 
       // console.log(window.responses.data[$scope.userScore.value].percentage);
@@ -180,7 +180,7 @@ app.controller('QuestionCtrl', ['$scope', '$timeout', '$window', '$http',
         // Log completion and score in GA
         $window.ga('send', 'event', 'Completions', 'Quiz Completed');
         $window.ga('send', 'event', 'Completions', 'Score',
-          $scope.userScore.value + ' out of 10');
+          $scope.userScore.value + ' out of 12');
         $window.ga('send', 'event', 'Completions', 'Score', 'Total Score',
           $scope.userScore.value);
       } else {
